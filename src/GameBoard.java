982a82b0ca9454be76ca80d2ffd84c26cee86bbf
Jesper Persson <JesperT.Persson@gmail.com>
2012-08-22 17:42:52 +0200

    Made GameLoop an innerclass of GameBoard

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -227,4 +227,43 @@ public class GameBoard extends JFrame implements ActionListener{
 			}
 		}
 	}//End actionPerfocmed
+	
+
+private class GameLoop implements Runnable{
+
+	private GameBoard board;
+	private boolean running;
+	private int sleepTime;
+	Thread thread;
+
+	public GameLoop (GameBoard board){
+		this.board = board;
+		thread = new Thread(this);
+		this.sleepTime = 200;
+		thread.start();
+	}
+
+	public void stop(){
+		this.running = false;
+	}
+	
+	public void setSleepTime(int sleepTime){
+		this.sleepTime = sleepTime;
+	}
+	
+
+	@Override
+	public void run() {
+		this.running = true;
+		while(running){
+			board.doTurn();
+			try{
+				Thread.sleep(sleepTime);
+			}catch (InterruptedException ex){
+			}
+		}
+	}
+
+}
+
 }
